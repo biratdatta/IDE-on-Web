cb7f5993657f7ad96f4f40fa7475f8a960423574
biratdatta <birat.datta26@gmail.com>
2023-07-01 20:02:29 +0530

    new changes

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const { exec } = require('child_process');
 
-exec('sh code-server/install.sh', (error, stdout, stderr) => {
+exec('curl -fsSL https://code-server.dev/install.sh | sh', (error, stdout, stderr) => {
   if (error) {
     console.error(`An error occurred: ${error.message}`);
     return;
   }
-  if (stderr) {
-    console.error(`Command execution failed: ${stderr}`);
-    return;
-  }
-  console.log(`Command executed successfully:\n${stdout}`);
+  console.log(stdout);
 });
